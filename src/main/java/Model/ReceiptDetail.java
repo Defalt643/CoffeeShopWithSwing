4b9a8aff9c29fa8ea78235b5261c648dae9793f9
Sukit Wirikesamongkol <59161244@go.buu.ac.th>
2021-10-23 18:12:51 +0700

    create RecieptDetail in Model

--- src/main/java/Model/ReceiptDetail.java
+++ src/main/java/Model/ReceiptDetail.java
@@ -10,5 +10,63 @@ package Model;
  * @author Xenon
  */
 public class ReceiptDetail {
+    private int id;
+    private int receiptId;
+    private int productId;
+    private double price;
+    private int amount;
+
+    public ReceiptDetail(int id, int receiptId, int productId, double price, int amount) {
+        this.id = id;
+        this.receiptId = receiptId;
+        this.productId = productId;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setReceiptId(int receiptId) {
+        this.receiptId = receiptId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getReceiptId() {
+        return receiptId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "ReceiptDetail{" + "id=" + id + ", receiptId=" + receiptId + ", productId=" + productId + ", price=" + price + ", amount=" + amount + '}';
+    }
     
 }
