7682a7220b2aeef291f5cca789ce785934a548ae
itthichai ongprateang <62160310@go.buu.ac.th>
2021-10-23 19:03:01 +0700

    AddClassTestProductInTest

---
+++ src/main/java/Test/TestProduct.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import DAO.ProductDAO;
+import Model.Product;
+
+/**
+ *
+ * @author Bunny0_
+ */
+public class TestProduct {
+
+    public static void main(String[] args) {
+        ProductDAO dao = new ProductDAO();
+        //TestInsert
+        int id = dao.add(new Product(-1, "green tea", 35.0));
+        System.out.println("id : " + id);
+        //TestInsertLastProdcut
+        Product lastProduct = dao.get(id);
+        System.out.println("last Product : "+ lastProduct);
+        //TestEditPrice
+        lastProduct.setPrice(120);
+        System.out.println("last Product Edit Price : "+lastProduct);
+        //TeestUpdateRowInProduct
+        int row = dao.update(lastProduct);
+        Product updateProduct = dao.get(id);
+        System.out.println("update product : "+updateProduct);
+        //TestDeleteProduct
+        dao.delete(id);
+        Product deleteProduct = dao.get(id);
+        System.out.println("delete product : "+ deleteProduct);
+    }
+}
