efceb6ab3eba6261f5d957608ef109480955a3d5
Sukit Wirikesamongkol <59161244@go.buu.ac.th>
2021-10-23 17:56:43 +0700

    User model created

---
+++ src/main/java/Model/User.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Xenon
+ */
+public class User {
+    int id;
+    String name;
+    String role;
+    String Username;
+    String Password;
+
+    public User(int id, String name, String role, String Username, String Password) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+        this.Username = Username;
+        this.Password = Password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name=" + name + ", role=" + role + ", Username=" + Username + ", Password=" + Password + '}';
+    }
+}
