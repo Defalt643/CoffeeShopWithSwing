9135c56b4abe40a818c0fcf3cfcf51079410bb48
Khummeung Wattanasaroj <62160244@go.buu.ac.th>
2021-10-23 17:45:43 +0700

    change modality type to MODELESS

--- src/main/java/UI/MainPanel.java
+++ src/main/java/UI/MainPanel.java
@@ -32,8 +32,7 @@ public class MainPanel extends javax.swing.JDialog {
         setTitle("Ok Coffee Shop");
         setMaximumSize(new java.awt.Dimension(900, 500));
         setMinimumSize(new java.awt.Dimension(900, 500));
-        setModal(true);
-        setUndecorated(true);
+        setModalityType(java.awt.Dialog.ModalityType.MODELESS);
         setPreferredSize(new java.awt.Dimension(900, 500));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
