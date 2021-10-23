2b4c2f6cabaf0b33b5049d718f82d868f5345baf
itthichai ongprateang <62160310@go.buu.ac.th>
2021-10-23 18:02:39 +0700

    Product add constructor

--- src/main/java/Model/Product.java
+++ src/main/java/Model/Product.java
@@ -13,7 +13,12 @@ public class Product {
     private int id;
     private String name;
     private double price;
-
+    
+    public Product(int id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
     public int getId() {
         return id;
     }
